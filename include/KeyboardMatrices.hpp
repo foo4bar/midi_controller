d2bb7e5f660ffd04093ebb1370408b994f909fab
Fedir Chernyavskyy <foo4bar@foo4bar.dev>
2022-01-12 18:34:48 +0100

    continued

--- include/KeyboardMatrices.hpp
+++ include/KeyboardMatrices.hpp
@@ -9,25 +9,28 @@
 
 namespace kbdmatrix
 {
+
     //See the circuit diagram for details regarding inputs/outputs configuration.
     inline constexpr uint8_t numberOfOutputPairsLeft{6};
     inline constexpr uint8_t numberOfOutputPairsRight{5};
     inline constexpr uint8_t numberOfInputs{8};
 
+    using KbdMatrix = KeyboardMatrixIO<numberOfOutputPairsLeft, numberOfInputs>;
+
     inline constexpr uint8_t numberOfScannedKeysLeft{numberOfOutputPairsLeft * numberOfInputs};
     inline constexpr uint8_t numberOfScannedKeysRight{numberOfOutputPairsRight * numberOfInputs};
     inline constexpr uint8_t numberOfScannedKeys{numberOfScannedKeysLeft + numberOfScannedKeysRight};
 
-    inline KeyboardMatrixIO keyboardMatrixIOLeft{KeyboardMatrixIO::Builder<numberOfOutputPairsLeft, numberOfInputs>{}
-                                                     .withFirstClosedContactsOutputs({50, 46, 42, 38, 37, 32})
-                                                     .withLastClosedContactsOutputs({52, 48, 44, 40, 36, 34})
-                                                     .withInputs({53, 51, 49, 47, 45, 43, 41, 39})
-                                                     .build()};
-    inline KeyboardMatrixIO keyboardMatrixIORight{KeyboardMatrixIO::Builder<numberOfOutputPairsRight, numberOfInputs>{}
-                                                      .withFirstClosedContactsOutputs({28, 24, 2, 6, 5})
-                                                      .withLastClosedContactsOutputs({30, 26, 22, 4, 7})
-                                                      .withInputs({35, 33, 31, 29, 27, 25, 23, 3})
-                                                      .build()};
+    inline KeyboardMatrixIO<numberOfOutputPairsLeft, numberOfInputs> keyboardMatrixIOLeft{KeyboardMatrixIO<numberOfOutputPairsLeft, numberOfInputs>::Builder<numberOfOutputPairsLeft, numberOfInputs>{}
+                                                                                              .withFirstClosedContactsOutputs({50, 46, 42, 38, 37, 32})
+                                                                                              .withLastClosedContactsOutputs({52, 48, 44, 40, 36, 34})
+                                                                                              .withInputs({53, 51, 49, 47, 45, 43, 41, 39})
+                                                                                              .build()};
+    inline KeyboardMatrixIO<numberOfOutputPairsRight, numberOfInputs> keyboardMatrixIORight{KeyboardMatrixIO<numberOfOutputPairsRight, numberOfInputs>::Builder<numberOfOutputPairsRight, numberOfInputs>{}
+                                                                                                .withFirstClosedContactsOutputs({28, 24, 2, 6, 5})
+                                                                                                .withLastClosedContactsOutputs({30, 26, 22, 4, 7})
+                                                                                                .withInputs({35, 33, 31, 29, 27, 25, 23, 3})
+                                                                                                .build()};
 
     const arduino::digital::InputStatePair getActualInstantaneousInputStatePair(const uint8_t keyNumber);
 }
--- include/KeyboardMatrixIO.hpp
+++ include/KeyboardMatrixIO.hpp
@@ -1,7 +1,6 @@
 #ifndef KeyboardMatrixIO_hpp
 #define KeyboardMatrixIO_hpp
 
-#include <vector>
 #include <array>
 
 #include <stdint.h>
@@ -10,69 +9,100 @@
 
 namespace kbdmatrix
 {
+    template <uint8_t numberOfOutputPairs, uint8_t numberOfInputs>
     class KeyboardMatrixIO
     {
     private:
         inline static constexpr uint8_t numberOfPins{54};
 
-        uint8_t numberOfOutputPairs;
-        uint8_t numberOfInputs;
-
         std::array<arduino::digital::Pin, numberOfPins> pins;
 
         //It's supposed that both outputs vectors are of the same size, i.e., numberOfOutputPairs. This is enforced by KeyboardMatrixIO::Builder.
-        std::vector<uint8_t> firstClosedContactsOutputs;
-        std::vector<uint8_t> lastClosedContactsOutputs;
+        std::array<uint8_t, numberOfOutputPairs> firstClosedContactsOutputs;
+        std::array<uint8_t, numberOfOutputPairs> lastClosedContactsOutputs;
         //It's supposed that the vectors is of size numberOfInputs. This is enforced by KeyboardMatrixIO::Builder.
-        std::vector<uint8_t> inputs;
+        std::array<uint8_t, numberOfInputs> inputs;
 
-        KeyboardMatrixIO(const uint8_t numberOfOutputPairs,
-                         const uint8_t numberOfInputs,
-                         const std::vector<uint8_t> &firstClosedContactsOutputs,
-                         const std::vector<uint8_t> &lastClosedContactsOutputs,
-                         const std::vector<uint8_t> &inputs);
+        KeyboardMatrixIO(const std::array<uint8_t, numberOfOutputPairs> &firstClosedContactsOutputs,
+                         const std::array<uint8_t, numberOfOutputPairs> &lastClosedContactsOutputs,
+                         const std::array<uint8_t, numberOfInputs> &inputs) : firstClosedContactsOutputs{firstClosedContactsOutputs},
+                                                                              lastClosedContactsOutputs{lastClosedContactsOutputs},
+                                                                              inputs{inputs}
+        {
+            std::array<arduino::digital::Pin, numberOfPins> pins;
+            for (uint8_t pinNumber = 0; pinNumber < numberOfPins; ++pinNumber)
+                pins[pinNumber] = arduino::digital::Pin{pinNumber};
 
-        const arduino::digital::State getInputState(const uint8_t outputToToggle, const uint8_t inputToCheck);
+            for (const auto &outputs : {firstClosedContactsOutputs, lastClosedContactsOutputs})
+                for (uint8_t outputToToggle : outputs)
+                {
+                    pins[outputToToggle].setMode(arduino::digital::Mode::output);
+                    pins[outputToToggle].setState(arduino::digital::State::high);
+                }
+
+            for (const uint8_t input : inputs)
+                pins[input].setMode(arduino::digital::Mode::inputWithInternalPullUp);
+
+            this->pins = pins;
+        }
+
+        const arduino::digital::State getInputState(const uint8_t outputToToggle, const uint8_t inputToCheck)
+        {
+            this->pins[outputToToggle].setState(arduino::digital::State::low);
+            auto result{this->pins[inputToCheck].getState()};
+            this->pins[outputToToggle].setState(arduino::digital::State::high);
+
+            return result;
+        }
 
     public:
-        const arduino::digital::InputStatePair getActualInstantaneousInputStatePair(const uint8_t contactPairNumber);
-
-        template <uint8_t numberOfOutputPairs, uint8_t numberOfInputs>
+        template <uint8_t numberOfOutputPairsBuilder, uint8_t numberOfInputsBuilder>
         class Builder
         {
         private:
-            std::vector<uint8_t> firstClosedContactsOutputs;
-            std::vector<uint8_t> lastClosedContactsOutputs;
-            std::vector<uint8_t> inputs;
+            std::array<uint8_t, numberOfOutputPairsBuilder> firstClosedContactsOutputs;
+            std::array<uint8_t, numberOfOutputPairsBuilder> lastClosedContactsOutputs;
+            std::array<uint8_t, numberOfInputsBuilder> inputs;
 
         public:
-            Builder &withFirstClosedContactsOutputs(const std::array<uint8_t, numberOfOutputPairs> &outputs)
+            Builder &withFirstClosedContactsOutputs(const std::array<uint8_t, numberOfOutputPairsBuilder> &outputs)
             {
-                this->firstClosedContactsOutputs = std::vector<uint8_t>{outputs.begin(), outputs.end()};
+                this->firstClosedContactsOutputs = outputs;
                 return *this;
             }
 
-            Builder &withLastClosedContactsOutputs(const std::array<uint8_t, numberOfOutputPairs> &outputs)
+            Builder &withLastClosedContactsOutputs(const std::array<uint8_t, numberOfOutputPairsBuilder> &outputs)
             {
-                this->lastClosedContactsOutputs = std::vector<uint8_t>{outputs.begin(), outputs.end()};
+                this->lastClosedContactsOutputs = outputs;
                 return *this;
             }
 
-            Builder &withInputs(const std::array<uint8_t, numberOfInputs> &inputs)
+            Builder &withInputs(const std::array<uint8_t, numberOfInputsBuilder> &inputs)
             {
-                this->inputs = std::vector<uint8_t>{inputs.begin(), inputs.end()};
+                this->inputs = inputs;
                 return *this;
             }
 
-            KeyboardMatrixIO build() const
+            KeyboardMatrixIO<numberOfOutputPairsBuilder, numberOfInputsBuilder> build() const
             {
-                return KeyboardMatrixIO{numberOfOutputPairs,
-                                        numberOfInputs,
-                                        this->firstClosedContactsOutputs,
-                                        this->lastClosedContactsOutputs,
-                                        this->inputs};
+                return KeyboardMatrixIO<numberOfOutputPairsBuilder, numberOfInputsBuilder>{this->firstClosedContactsOutputs,
+                                                                                           this->lastClosedContactsOutputs,
+                                                                                           this->inputs};
             }
         };
+
+        const arduino::digital::InputStatePair getActualInstantaneousInputStatePair(const uint8_t contactPairNumber)
+        {
+            const uint8_t outputNumber{(uint8_t)(contactPairNumber / numberOfInputs)};
+            const uint8_t firstClosedContactsOutput{this->firstClosedContactsOutputs[outputNumber]};
+            const uint8_t lastClosedContactsOutput{this->lastClosedContactsOutputs[outputNumber]};
+
+            const uint8_t inputNumber{(uint8_t)(contactPairNumber % numberOfInputs)};
+            const uint8_t input{this->inputs[inputNumber]};
+
+            return arduino::digital::InputStatePair{getInputState(firstClosedContactsOutput, input),
+                                                    getInputState(lastClosedContactsOutput, input)};
+        }
     };
 }
 
