2a09195b9543a17096b4d6ce2d64c7ba762df1da
Fedir Chernyavskyy <foo4bar@foo4bar.dev>
2022-01-22 23:05:32 +0100

    continued

--- src/main.cpp
+++ src/main.cpp
@@ -9,48 +9,87 @@
 #include "KeyboardController.hpp"
 #include "DigitalIO.hpp"
 
-#ifndef AVR_STUB_DEBUG
-MIDI_CREATE_DEFAULT_INSTANCE()
-#else
+using Pin = arduino::digital::Pin;
+using State = arduino::digital::State;
+using Mode = arduino::digital::Mode;
+using MidiInterface = midi::MidiInterface<midi::SerialMIDI<HardwareSerial>>;
+
+void initAvrStubDebug();
+void attachUsbDevice();
+kbd::KeyboardMatricesIO initKeyboardMatrices();
+kbd::KeyboardController initKeyboardController(kbd::KeyboardMatricesIO &, MidiInterface &);
+std::vector<Pin> initOutputsWithState(const std::vector<uint8_t>, const State);
+std::vector<Pin> initInputsPulledUp(const std::vector<uint8_t>);
+void sendMidiEvents(const kbd::KeyboardController);
+void serialEventSafeRun();
+
+int main()
+{
+    init();
+    initAvrStubDebug();
+    attachUsbDevice();
+
+//This works either globally or in main(). It cannot be extracted to a function for some reason.
+#ifdef AVR_STUB_DEBUG
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI)
+#else
+MIDI_CREATE_DEFAULT_INSTANCE()
+MIDI.begin(MIDI_CHANNEL_OMNI);
 #endif
 
-// Copied as is from the Arduino framework main() implementation.
-void attachUsbDeviceIfAny()
+    auto keyboardMatrices{initKeyboardMatrices()};
+
+    sendMidiEvents(initKeyboardController(keyboardMatrices, MIDI));
+    serialEventSafeRun();
+}
+
+void initAvrStubDebug()
 {
-#if defined(USBCON)
+#ifdef AVR_STUB_DEBUG
+    debug_init();
+#endif
+}
+
+void attachUsbDevice()
+{
+// Copied as is from the Arduino framework main() implementation.
+#ifdef USBCON
     USBDevice.attach();
 #endif
 }
 
-void initMidiInterface()
+kbd::KeyboardMatricesIO initKeyboardMatrices()
 {
-#ifndef AVR_STUB_DEBUG
-    MIDI.begin(MIDI_CHANNEL_OMNI);
-#endif
+    return kbd::KeyboardMatricesIO{{kbd::KeyboardMatrixIO::Builder{
+                                        .firstClosedContactsOutputs{initOutputsWithState({50, 46, 42, 38, 37, 32}, State::high)},
+                                        .lastClosedContactsOutputs{initOutputsWithState({52, 48, 44, 40, 36, 34}, State::high)},
+                                        .inputs{initInputsPulledUp({53, 51, 49, 47, 45, 43, 41, 39})},
+                                        .numberOfKeysBeingScanned{48}}
+                                        .build(),
+                                    kbd::KeyboardMatrixIO::Builder{
+                                        .firstClosedContactsOutputs{initOutputsWithState({28, 24, 2, 6, 5}, State::high)},
+                                        .lastClosedContactsOutputs{initOutputsWithState({30, 26, 22, 4, 7}, State::high)},
+                                        .inputs{initInputsPulledUp({35, 33, 31, 29, 27, 25, 23, 3})},
+                                        .numberOfKeysBeingScanned{40}}
+                                        .build()}};
 }
 
-std::vector<arduino::digital::Pin> initInputsPulledUp(const std::vector<uint8_t> pinNumbers)
+kbd::KeyboardController initKeyboardController(kbd::KeyboardMatricesIO &keyboardMatrices, MidiInterface &midiInterface)
 {
-    std::vector<arduino::digital::Pin> pins;
-    for (const uint8_t pinNumber : pinNumbers)
-    {
-        const arduino::digital::Pin &pin{pinNumber};
-        pin.setMode(arduino::digital::Mode::inputWithInternalPullUp);
-        pins.push_back(pin);
-    }
-
-    return pins;
+    return kbd::KeyboardController::Builder{.firstKeyMidiNoteNumber{21},
+                                            .midiChannel{1},
+                                            .keyboardMatrices{keyboardMatrices},
+                                            .midiInterface{midiInterface}}
+        .build();
 }
 
-std::vector<arduino::digital::Pin> initOutputsWithState(const std::vector<uint8_t> pinNumbers,
-                                                        const arduino::digital::State state)
+std::vector<Pin> initOutputsWithState(const std::vector<uint8_t> pinNumbers, const State state)
 {
-    std::vector<arduino::digital::Pin> pins;
+    std::vector<Pin> pins;
     for (const uint8_t pinNumber : pinNumbers)
     {
-        const arduino::digital::Pin &pin{pinNumber};
-        pin.setMode(arduino::digital::Mode::output);
+        const Pin &pin{pinNumber};
+        pin.setMode(Mode::output);
         pin.setState(state);
         pins.push_back(pin);
     }
@@ -58,52 +97,21 @@ std::vector<arduino::digital::Pin> initOutputsWithState(const std::vector<uint8_
     return pins;
 }
 
-// Copied as is from the Arduino framework main() implementation.
-void serialEventSafeRun()
+std::vector<Pin> initInputsPulledUp(const std::vector<uint8_t> pinNumbers)
 {
-    if (serialEventRun)
+    std::vector<Pin> pins;
+    for (const uint8_t pinNumber : pinNumbers)
     {
-        serialEventRun();
+        const Pin &pin{pinNumber};
+        pin.setMode(Mode::inputWithInternalPullUp);
+        pins.push_back(pin);
     }
+
+    return pins;
 }
 
-int main()
+void sendMidiEvents(kbd::KeyboardController keyboardController)
 {
-    // Copied as is from the Arduino framework main() implementation.
-    init();
-
-#ifdef AVR_STUB_DEBUG
-    debug_init();
-#endif
-
-    attachUsbDeviceIfAny();
-
-    initMidiInterface();
-
-    using State = arduino::digital::State;
-    auto keyboardMatrices{kbd::KeyboardMatricesIO{{kbd::KeyboardMatrixIO::Builder{
-                                                       .firstClosedContactsOutputs{initOutputsWithState({50, 46, 42, 38, 37, 32},
-                                                                                                        State::high)},
-                                                       .lastClosedContactsOutputs{initOutputsWithState({52, 48, 44, 40, 36, 34},
-                                                                                                       State::high)},
-                                                       .inputs{initInputsPulledUp({53, 51, 49, 47, 45, 43, 41, 39})},
-                                                       .numberOfKeysBeingScanned{48}}
-                                                       .build(),
-                                                   kbd::KeyboardMatrixIO::Builder{
-                                                       .firstClosedContactsOutputs{initOutputsWithState({28, 24, 2, 6, 5},
-                                                                                                        State::high)},
-                                                       .lastClosedContactsOutputs{initOutputsWithState({30, 26, 22, 4, 7},
-                                                                                                       State::high)},
-                                                       .inputs{initInputsPulledUp({35, 33, 31, 29, 27, 25, 23, 3})},
-                                                       .numberOfKeysBeingScanned{40}}
-                                                       .build()}}};
-
-    auto keyboardController{kbd::KeyboardController::Builder{.firstKeyMidiNoteNumber{21},
-                                                             .midiChannel{1},
-                                                             .keyboardMatrices{keyboardMatrices},
-                                                             .midiInterface{MIDI}}
-                                .build()};
-
 #ifdef CYCLE_TIME_DEBUG_MESSAGES
     long counter{};
     char buffer[100];
@@ -124,6 +132,13 @@ int main()
         }
 #endif
     }
+}
 
-    serialEventSafeRun();
+void serialEventSafeRun()
+{
+    // Copied as is from the Arduino framework main() implementation.
+    if (serialEventRun)
+    {
+        serialEventRun();
+    }
 }
